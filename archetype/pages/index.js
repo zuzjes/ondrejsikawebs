8d9b20f9b140e6f3bbed7523bc125ea9815bf345
Ondrej Sika <ondrej@ondrejsika.com>
2019-04-09 21:54:34 +0200

    archetype,archetype-theme - add example components

--- archetype/pages/index.js
+++ archetype/pages/index.js
@@ -1,7 +1,19 @@
 import '../css'
+import Footer from '../../archetype-theme/components/Footer'
+import Alert from '../../archetype-theme/components/Alert'
+import Person from '../../archetype-theme/components/Person'
 
 const Index = () => (
-  <h1>Ondrej Sika Webs / Archetype</h1>
+  <div>
+    <h1>Ondrej Sika Webs / Archetype</h1>
+    <Alert>Hello</Alert>
+    <Person
+      name='Ondrej Sika'
+      imgSrc='https://ondrejsika.com/ondrej/photo/ondrejsika3_200px.jpg'
+      bio='Hacker from Prague'
+    />
+    <Footer />
+  </div>
 )
 
 export default Index
