ed3cad3c439501d3a078ac0543df8fb39d8bd96e
Ondrej Sika <ondrej@ondrejsika.com>
2019-04-09 20:05:03 +0200

    archetype - add CSS & Bootstrap

--- archetype/next.config.js
+++ archetype/next.config.js
@@ -5,3 +5,6 @@ module.exports = {
     }
   }
 }
+
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS(module.exports)
--- archetype/pages/index.js
+++ archetype/pages/index.js
@@ -1,3 +1,6 @@
+import "../css/index.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+
 const Index = () => (
   <h1>Ondrej Sika Webs / Archetype</h1>
 )
